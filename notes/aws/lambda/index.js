c3053bd3201aa7dcac1729854610c7998c4447e2
Taylor McKenna <tamr43@gmail.com>
2018-03-01 07:49:34 -0600

    Update index.js

--- notes/aws/lambda/index.js
+++ notes/aws/lambda/index.js
@@ -1,11 +1,29 @@
 exports.handler = (event, context, callback) => {
     var input = event.queryStringParameters.input;
+    //Example Input URL Query String:
+    //?input=2017006,2,03/01/2018,40000,M
+    //Insured Number,Period Number, Effective Date, Premium, Underwriting Company(M=>MEM, P=>PRV)
+    /////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    //Requirements:
+    //The output JSON message should have the following:
+    //Policy Number: UW Compnay + InsuredNumber + PeriodNumber (ie. "MEM2017006-02")
+    //Effective Date as YYYYMMDD
+    //Premium Amount with $
+    //
     /////////////////////////////////////
     //DON"T CHANGE ANYTHING ABOVE HERE!!!
     /////////////////////////////////////
     
+    
+    
+    
     //Implement Code Here
     
+    
+    
+    
+    //Output Variables
     var policyNumber = "";
     var effectiveDate = "";
     var premiumAmount = 0;
